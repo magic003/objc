6d8a076cf8513667cdc66d3b8719bacb38bf4b7b
Minjie Zha <minjiezha@gmail.com>
2023-09-03 06:55:39 -0700

    add comments to Bool

--- runtime/bool.v
+++ runtime/bool.v
@@ -1,7 +1,10 @@
 module runtime
 
+// A type that represents a Boolean value in Objective-C.
 pub type Bool = bool
 
+// yes as true.
 pub const yes = Bool(true)
 
+// no as false.
 pub const no = Bool(false)
