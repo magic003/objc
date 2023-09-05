a661746ae7d4c0c462ec00bed902b3bf93de8045
Minjie Zha <minjiezha@gmail.com>
2023-09-04 22:34:13 -0700

    add send message unit tests for 0 and 1 argument

--- runtime/message_test.v
+++ runtime/message_test.v
@@ -1,5 +1,33 @@
 module runtime
 
+fn test_send_message_0_argument() {
+	cls := Class.get('NSMutableArray') or { panic('failed to load class NSMutableArray') }
+	arr := cls.message(Sel.get('alloc')).request[Id]()
+		.message(Sel.get('init')).request[Id]()
+	obj_cls := Class.get('NSObject') or { panic('failed to load class NSObject') }
+	obj := obj_cls.message(Sel.get('new')).request[Id]()
+	arr.message(Sel.get('addObject:')).args1(obj).notify()
+	mut size := arr.message(Sel.get('count')).request[u64]()
+	assert size == 1
+
+	arr.message(Sel.get('removeAllObjects')).notify()
+	size = arr.message(Sel.get('count')).request[u64]()
+	assert size == 0
+}
+
+fn test_send_message_1_argument() {
+	obj_cls := Class.get('NSObject') or { panic('failed to load class NSObject') }
+	obj := obj_cls.message(Sel.get('new')).request[Id]()
+
+	cls := Class.get('NSSet') or { panic('failed to load class NSSet') }
+	s := cls.message(Sel.get('setWithObject:')).args1[voidptr](obj).request[Id]()
+	size := s.message(Sel.get('count')).request[u64]()
+	assert size == 1
+
+	contains := s.message(Sel.get('containsObject:')).args1(obj).request[Bool]()
+	assert contains == yes
+}
+
 fn test_send_message_ns_view() {
 	cls := Class.get('NSView') or { panic('failed to load class NSView') }
 	rect := CGRect{
